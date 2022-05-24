1acc5a04678c7d222bbf1a96f640afdba3fbd1d1
CAIO MUCIO FURTADO DOS SANTOS FILHO <caiomucio54@gmail.com>
2022-05-23 23:56:14 -0300

    Primeiro commit

---
+++ cypress/integration/arrow.spec.js
@@ -0,0 +1,21 @@
+it ('nada', function() {} )
+
+// function soma(a,b) {
+//     return a + b;
+// }
+
+// const soma = function(a,b){
+//     return(a+b)
+// }
+
+// const soma = (a,b) => {
+//     return(a+b)
+// }
+
+ const soma = (a,b) => a + b
+
+// cont soma = a => a + a pode ser somado sem o parêntese porque é mesmo elemento
+
+
+
+console.log(soma(1,4))
